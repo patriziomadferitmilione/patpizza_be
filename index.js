0ea1e4f0d2682cd175df0938ef0e148e3ffccdba
Patrizio Milione <patriziomilione@gmail.com>
2023-07-14 17:04:22 +0200

    bluetooth

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const cors = require("cors");
 const mongoString = process.env.DATABASE_URL;
 const routes = require("./routes/routes");
 
-const bluetooth = require("./bluetooth");
-
 mongoose.connect(mongoString);
 const database = mongoose.connection;
 
@@ -25,13 +23,6 @@ app.use(cors()); // Add CORS middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/", routes);
-
-// Endpoint to trigger Bluetooth functionality
-app.get("/trigger-bluetooth", (req, res) => {
-  const deviceUUID = '0000110C-0000-1000-8000-00805F9B34FB'; // Replace with the UUID of your BLE device
-  bluetooth.start(deviceUUID);
-  res.send("Bluetooth functionality triggered.");
-});
 
 app.listen(3000, () => {
   console.log("Server Started at 3000");
