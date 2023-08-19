3321540667150fd8894c2378d5a1d21aac2d5cc0
Patrizio Milione <patriziomilione@gmail.com>
2023-08-19 13:28:54 +0200

    daje

--- routes/ordine.js
+++ routes/ordine.js
@@ -1,116 +1,110 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 const Ordine = require('../models/ordineModel')
 
 //Post Method
 router.post('/newOrdine', async (req, res) => {
-    const data = new Ordine({
-        customer_id: req.body.customer_id,
-        indirizzo: req.body.indirizzo,
-        nomeCampanello: req.body.nomeCampanello,
-        cellulare: req.body.cellulare,
-        orarioConsegna: req.body.orarioConsegna,
-        metodoPagamento: req.body.metodoPagamento,
-        zona: req.body.zona,
-        note: req.body.note,
-        createdAt: req.body.createdAt
-    })
+  const data = new Ordine({
+    customer_id: req.body.customer_id,
+    indirizzo: req.body.indirizzo,
+    nomeCampanello: req.body.nomeCampanello,
+    cellulare: req.body.cellulare,
+    orarioConsegna: req.body.orarioConsegna,
+    metodoPagamento: req.body.metodoPagamento,
+    zona: req.body.zona,
+    note: req.body.note,
+    createdAt: req.body.createdAt,
+  })
 
-    try {
-        const dataToSave = await data.save();
-        res.status(200).json({ _id: dataToSave._id, ...dataToSave._doc });
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
+  try {
+    const dataToSave = await data.save()
+    res.status(200).json({ _id: dataToSave._id, ...dataToSave._doc })
+  } catch (error) {
+    console.error(error) // This will print the complete error object
+    res.status(400).json({ message: error.message })
+  }
 })
 
 //Get all Method
 router.get('/getOrdini', async (req, res) => {
-    try {
-        const data = await Ordine.find().sort({ zona: 'asc' }); // Sort in ascending order by 'nome' property
-        res.json(data);
-      } catch (error) {
-        res.status(500).json({ message: error.message });
-      }
+  try {
+    const data = await Ordine.find().sort({ zona: 'asc' }) // Sort in ascending order by 'nome' property
+    res.json(data)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
 })
 
 //Get all from today and how many per time slot
 router.get('/getOrdiniToday', async (req, res) => {
-    try {
-      // Get today's date
-      const today = new Date();
-      today.setHours(0, 0, 0, 0); // Set the time to midnight
-  
-      // Find records with 'createdAt' set to today
-      const data = await Ordine.find({ createdAt: { $gte: today } });
-  
-      // Count records for each 'orarioConsegna' enum possible values
-      const counts = {};
-      data.forEach(ordine => {
-        if (ordine.orarioConsegna in counts) {
-          counts[ordine.orarioConsegna]++;
-        } else {
-          counts[ordine.orarioConsegna] = 1;
-        }
-      });
-  
-      // Sort the counts object by key in chronological order
-      const sortedCounts = Object.fromEntries(
-        Object.entries(counts).sort((a, b) => {
-          const aTime = Number(a[0].replace(':', ''));
-          const bTime = Number(b[0].replace(':', ''));
-          return aTime - bTime;
-        })
-      );
-  
-      res.json(sortedCounts);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  });
+  try {
+    // Get today's date
+    const today = new Date()
+    today.setHours(0, 0, 0, 0) // Set the time to midnight
+
+    // Find records with 'createdAt' set to today
+    const data = await Ordine.find({ createdAt: { $gte: today } })
+
+    // Count records for each 'orarioConsegna' enum possible values
+    const counts = {}
+    data.forEach((ordine) => {
+      if (ordine.orarioConsegna in counts) {
+        counts[ordine.orarioConsegna]++
+      } else {
+        counts[ordine.orarioConsegna] = 1
+      }
+    })
+
+    // Sort the counts object by key in chronological order
+    const sortedCounts = Object.fromEntries(
+      Object.entries(counts).sort((a, b) => {
+        const aTime = Number(a[0].replace(':', ''))
+        const bTime = Number(b[0].replace(':', ''))
+        return aTime - bTime
+      })
+    )
+
+    res.json(sortedCounts)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+})
 
 //Get by ID Method
 router.get('/getOrdine/:id', async (req, res) => {
-    try{
-        const data = await Ordine.findById(req.params.id);
-        res.json(data)
-    }
-    catch(error){
-        res.status(500).json({message: error.message})
-    }
+  try {
+    const data = await Ordine.findById(req.params.id)
+    res.json(data)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
 })
 
 //Update by ID Method
 router.patch('/updateOrdine/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const updatedData = req.body;
-        const options = { new: true };
+  try {
+    const id = req.params.id
+    const updatedData = req.body
+    const options = { new: true }
 
-        const result = await Ordine.findByIdAndUpdate(
-            id, updatedData, options
-        )
+    const result = await Ordine.findByIdAndUpdate(id, updatedData, options)
 
-        res.send(result)
-    }
-    catch (error) {
-        res.status(400).json({ message: error.message })
-    }
+    res.send(result)
+  } catch (error) {
+    res.status(400).json({ message: error.message })
+  }
 })
 
 //Delete by ID Method
 router.delete('/deleteOrdine/:id', async (req, res) => {
-    try {
-        const id = req.params.id;
-        const data = await Ordine.findByIdAndDelete(id)
-        res.send(`Document with ${data.name} has been deleted..`)
-    }
-    catch (error) {
-        res.status(400).json({ message: error.message })
-    }
+  try {
+    const id = req.params.id
+    const data = await Ordine.findByIdAndDelete(id)
+    res.send(`Document with ${data.name} has been deleted..`)
+  } catch (error) {
+    res.status(400).json({ message: error.message })
+  }
 })
 
-
-
 // Export the router
-module.exports = router;
+module.exports = router
