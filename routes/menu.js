62dc62503b739340be6c2fad60bfbe9e7d4ca35f
Patrizio Milione <patriziomilione@gmail.com>
2023-09-07 14:46:14 +0200

    daje

--- routes/menu.js
+++ routes/menu.js
@@ -1,70 +1,70 @@
-const express = require("express");
-const router = express.Router();
-const Menu = require("../models/menuModel");
+const express = require('express')
+const router = express.Router()
+const Menu = require('../models/menuModel')
 
 //Post Method
-router.post("/newMenu", async (req, res) => {
+router.post('/newMenu', async (req, res) => {
   const data = new Menu({
     nome: req.body.nome,
     ingredienti: req.body.ingredienti,
     categoria: req.body.categoria,
     note: req.body.note,
     createdAt: req.body.createdAt,
-  });
+  })
 
   try {
-    const dataToSave = await data.save();
-    res.status(200).json({ _id: dataToSave._id, ...dataToSave._doc });
+    const dataToSave = await data.save()
+    res.status(200).json({ _id: dataToSave._id, ...dataToSave._doc })
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    res.status(400).json({ message: error.message })
   }
-});
+})
 
 //Get all Method
-router.get("/getMenu", async (req, res) => {
-    try {
-        const data = await Menu.find().sort({ nome: 'asc' }); // Sort in ascending order by 'nome' property
-        res.json(data);
-      } catch (error) {
-        res.status(500).json({ message: error.message });
-      }
-});
+router.get('/getMenu', async (req, res) => {
+  try {
+    const data = await Menu.find().sort({ nome: 'asc' }) // Sort in ascending order by 'nome' property
+    res.json(data)
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+})
 
 //Get by ID Method
-router.get("/getMenuItem/:id", async (req, res) => {
+router.get('/getMenuItem/:id', async (req, res) => {
   try {
-    const data = await Menu.findById(req.params.id);
-    res.json(data);
+    const data = await Menu.findById(req.params.id)
+    res.json(data)
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message })
   }
-});
+})
 
 //Update by ID Method
-router.patch("/updateMenu/:id", async (req, res) => {
+router.patch('/updateMenu/:id', async (req, res) => {
   try {
-    const id = req.params.id;
-    const updatedData = req.body;
-    const options = { new: true };
+    const id = req.params.id
+    const updatedData = req.body
+    const options = { new: true }
 
-    const result = await Menu.findByIdAndUpdate(id, updatedData, options);
+    const result = await Menu.findByIdAndUpdate(id, updatedData, options)
 
-    res.send(result);
+    res.send(result)
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    res.status(400).json({ message: error.message })
   }
-});
+})
 
 //Delete by ID Method
-router.delete("/deleteMenu/:id", async (req, res) => {
+router.delete('/deleteMenu/:id', async (req, res) => {
   try {
-    const id = req.params.id;
-    const data = await Menu.findByIdAndDelete(id);
-    res.send(`Document with ${data.name} has been deleted..`);
+    const id = req.params.id
+    const data = await Menu.findByIdAndDelete(id)
+    res.send(`Document with ${data.name} has been deleted..`)
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    res.status(400).json({ message: error.message })
   }
-});
+})
 
 // Export the router
-module.exports = router;
+module.exports = router
